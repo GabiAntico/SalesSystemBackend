935874f830adf6e427169e4efe742aa21ee73b70
gabrielantico <412459@tecnicatura.frc.utn.edu.ar>
2025-08-04 22:47:43 -0300

    salesService getAllSales test

---
+++ src/test/java/com/sales/api/services/implementations/SalesServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.sales.api.services.implementations;
+
+import com.sales.api.dtos.SaleDto;
+import com.sales.api.entities.*;
+import com.sales.api.repositories.SalesRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class SalesServiceImplTest {
+
+    @Autowired
+    private SalesServiceImpl salesService;
+
+    @MockitoBean
+    private SalesRepository salesRepository;
+
+    @Test
+    void getAllSales_return_SaleDto_list() {
+        when(salesRepository.findAll()).thenReturn(List.of(
+                new Sale(1L,
+                        new Client(1L, "client1"),
+                        new Seller(1L, "seller1"),
+                        List.of(
+                        new SaleDetail(1L,
+                                new Product(1L, "product1", new BigDecimal(100)),
+                                10,
+                                new BigDecimal(100),
+                                new BigDecimal(1000),
+                                new Sale(1L, null, null, null, null))
+                ),
+                        new BigDecimal(1000))
+        ));
+
+        List<SaleDto> sales = salesService.getAllSales();
+
+        assertEquals(SaleDto.class, sales.get(0).getClass());
+    }
+
+    @Test
+    void postSale() {
+    }
+
+    @Test
+    void getSalesByClient() {
+    }
+}
